028bde03c831bc2813c725c1f5cc45f01d738314
BLAz3-1 <ikramnawazkhan222@gmail.com>
2023-05-15 21:02:00 +0530

    first commit

---
+++ COMPRESSOR_AND_DECOMPRESSOR/src/main/java/com/mycompany/compressor_and_decompressor/COMPRESSOR_AND_DECOMPRESSOR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compressor_and_decompressor;
+
+/**
+ *
+ * @author ikram
+ */
+public class COMPRESSOR_AND_DECOMPRESSOR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
